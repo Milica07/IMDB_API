1d0c5615398f6ee7d7ca95bfa01883b0086c8a72
Milica Pavlovic <milicapavlovic@Milicas-MacBook-Pro.local>
2022-05-22 20:38:01 +0200

    #CSDF-106 Authentication

---
+++ imdb_api/users/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('email', 'name', 'is_staff')
+    list_filter = ('email', 'name', 'is_staff')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'name', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+
+    ordering = ('email',)
+    search_fields = ('email',)
+
+
+admin.site.register(User, CustomUserAdmin)
