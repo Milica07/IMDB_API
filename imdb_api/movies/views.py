315b59877701539c5c32e75400860c31422d6c1e
Milica Pavlovic <milicapavlovic@Milicas-MacBook-Pro.local>
2022-06-03 10:34:56 +0200

    Movie Filter

--- imdb_api/movies/views.py
+++ imdb_api/movies/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, mixins, filters
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Movie
 from .serializers import MovieSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 class MovieViewSet(mixins.ListModelMixin,
                 mixins.RetrieveModelMixin,
@@ -11,7 +12,8 @@ class MovieViewSet(mixins.ListModelMixin,
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
     permission_classes = [AllowAny]
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
     search_fields = ['title'] 
+    filterset_fields = ['genre'] 
 
     
